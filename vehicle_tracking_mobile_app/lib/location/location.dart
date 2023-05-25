0f00460b9bbac6be1d11c092b9e93d566ca46904
Younis Yousaf <89453509+Younis07@users.noreply.github.com>
2023-05-25 19:07:14 +0500

    made some changes in 'location.dart' file, sending user info to the server

--- vehicle_tracking_mobile_app/lib/location/location.dart
+++ vehicle_tracking_mobile_app/lib/location/location.dart
@@ -1,250 +1,138 @@
-// ignore_for_file: avoid_print
+// Add the 'dart:io' import statement
+// ignore_for_file: avoid_print, library_private_types_in_public_api
 
 import 'dart:convert';
-// import 'dart:html';
+import 'dart:io';
 
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart' as perm;
 
 class CarLocation extends StatefulWidget {
-  const CarLocation({Key? key}) : super(key: key);
+  final String serverIP;
+  final String serverPort;
+
+  const CarLocation({
+    Key? key,
+    required this.serverIP,
+    required this.serverPort,
+  }) : super(key: key);
 
   @override
   _CarLocationState createState() => _CarLocationState();
 }
 
 class _CarLocationState extends State<CarLocation> {
-  late GoogleMapController mapController;
+  GoogleMapController? mapController;
+  List<Marker> markers = [];
+  LocationData? currentLocation;
+  Location location = Location();
+  StreamSubscription<LocationData>? locationSubscription;
+  Socket? socket;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  bool showMap = false;
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  void _startTracking() {
-    setState(() {
-      showMap = true;
+  @override
+  void initState() {
+    super.initState();
+    checkLocationPermissions();
+    locationSubscription =
+        location.onLocationChanged.listen((LocationData locationData) {
+      setState(() {
+        currentLocation = locationData;
+        updateMarker();
+        sendDataToServer();
+      });
     });
   }
 
-  var ip;
   @override
-  void initState() {
-    getDirectLocation();
-    super.initState();
+  void dispose() {
+    locationSubscription?.cancel();
+    socket?.close();
+    super.dispose();
   }
 
-  var locationparse;
-  Future getIsp() async {
-    var response = await http.get(
-      Uri.parse("https://api.ipify.org/"),
-    );
-    print(response.body);
-    ip = response.body;
-  }
-
-  Future getLocation() async {
-    var response = await http.get(
-      Uri.parse("http://ip-api.com/json/${ip}"),
-    );
-    var jsonparse = json.decode(response.body);
-    locationparse = jsonparse;
-    print(jsonparse);
-  }
-
-  getDirectLocation() async {
-    await getIsp();
-    await getLocation();
-  }
-
-  void sendLocation(double latitude, double longitude) async {
-    const url =
-        'https://eouvt62yg2e9pky.m.pipedream.net'; // Replace with the Pipedream URL or any other temporary endpoint
-    final data = {
-      'latitude': latitude.toString(),
-      'longitude': longitude.toString(),
-    };
-
-    try {
-      final response = await http.post(Uri.parse(url), body: data);
-
-      if (response.statusCode == 200) {
-        print('Location sent successfully');
-      } else {
-        print('Error sending location: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error sending location: $e');
+  Future<void> checkLocationPermissions() async {
+    final perm.PermissionStatus status =
+        await perm.Permission.location.request();
+    if (status != perm.PermissionStatus.granted) {
+      print("Your Location is not granted, Turn on your Location");
     }
   }
-  // void locationInfo() {
-  //   Text('country:${locationparse['country']}');
-  //   Text('countryCode:${locationparse['countryCode']}');
-  //   Text('region:${locationparse['region']}');
-  //   Text('regionName:${locationparse['regionName']}');
-  //   Text('city:${locationparse['city']}');
-  //   Text('timezone:${locationparse['timezone']}');
-  //   Text('isp:${locationparse['isp']}');
-  // }
+
+  void updateMarker() {
+    if (currentLocation != null) {
+      final LatLng newPosition =
+          LatLng(currentLocation!.latitude!, currentLocation!.longitude!);
+      setState(() {
+        markers = [
+          Marker(
+            markerId: const MarkerId('currentLocation'),
+            position: newPosition,
+          ),
+        ];
+        mapController?.animateCamera(CameraUpdate.newLatLng(newPosition));
+      });
+    }
+  }
+
+  Future<void> sendDataToServer() async {
+    try {
+      if (socket == null || socket?.write == null) {
+        // Establish a new TCP connection
+        socket =
+            await Socket.connect(widget.serverIP, int.parse(widget.serverPort));
+      }
+
+      final locationData = currentLocation!;
+      final data =
+          'Location: ${locationData.latitude},${locationData.longitude}';
+
+      // Send data to the server
+      socket?.write(data);
+
+      // Optionally, wait for a response from the server
+      final response = await socket
+          ?.transform(utf8.decoder as StreamTransformer<Uint8List, dynamic>)
+          .join();
+      print('Server response: $response');
+    } catch (e) {
+      print('Error connecting to the server: $e');
+      socket?.close();
+      socket = null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: showMap
-            ? AppBar(
-                backgroundColor: const Color.fromARGB(255, 27, 187, 1),
-                title: const Center(
-                  child: Text('Google Maps'),
-                ),
-                elevation: 2,
-              )
-            : null,
-        body: Stack(
-          children: [
-            if (showMap)
-              GoogleMap(
-                myLocationButtonEnabled: true,
-                myLocationEnabled: true,
-                // mapType: MapType.normal,
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 11.0,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Vehicle Tracking")),
+        backgroundColor: const Color.fromARGB(255, 27, 187, 1),
+        leading: null,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              zoomGesturesEnabled: true,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(27.7089427, 85.3086209),
+                zoom: 15.0,
               ),
-            // Center(
-            //   child: Column(
-            //     children: [
-
-            //     ],
-            //   ),
-            // ),
-            if (!showMap)
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 250,
-                      height: 400,
-                      margin: const EdgeInsets.only(top: 200),
-                      child: Image.asset('assets/images/Car.png'),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(
-                        bottom: 15,
-                      ),
-                      child: Positioned(
-                        bottom: 10,
-                        top: 90,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // getIsp();
-                            // getLocation();
-                            getDirectLocation();
-                            _startTracking();
-                            sendLocation(_center.latitude, _center.longitude);
-                            // locationInfo();
-                          },
-                          //  getIsp();,
-                          style: ElevatedButton.styleFrom(
-                            shape: const CircleBorder(),
-                            padding: const EdgeInsets.all(40.0),
-                            backgroundColor:
-                                const Color.fromARGB(255, 27, 187, 1),
-                          ),
-                          child: const Text(
-                            '\t\t\t\tStart \n Tracking',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-          ],
-        ),
+              mapType: MapType.normal,
+              onMapCreated: (controller) {
+                setState(() {
+                  mapController = controller;
+                });
+              },
+              markers: Set<Marker>.from(markers),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-// import 'dart:collection';
-
-// import 'package:flutter/material.dart';
-// import 'package:geojson_vi/geojson_vi.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-// class CarLocation extends StatelessWidget {
-//   const CarLocation({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Home(),
-//     );
-//   }
-// }
-
-// class Home extends StatefulWidget {
-//   const Home({super.key});
-
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   GoogleMapController? mapController;
-//   List<Marker> markers = [];
-//   Set<Polygon> _polygon = HashSet<Polygon>();
-//   static const LatLng showLocation = LatLng(27.7089427, 85.3086209);
-//   List<LatLng> points = [];
-//   GeoJSONPolygon? geoJSONPolygon;
-//   num? area;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Center(child: Text("Google Map")),
-//         backgroundColor: const Color.fromARGB(255, 27, 187, 1),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: GoogleMap(
-//               zoomGesturesEnabled: true,
-//               myLocationButtonEnabled: true,
-//               myLocationEnabled: true,
-//               initialCameraPosition: const CameraPosition(
-//                 target: showLocation,
-//                 zoom: 15.0,
-//               ),
-//               mapType: MapType.normal,
-//               onMapCreated: (controller) {
-//                 setState(() {
-//                   mapController = controller;
-//                 });
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
---
+++ vehicle_tracking_mobile_app/lib/pages/start_tracking.dart
@@ -0,0 +1,230 @@
+// ignore_for_file: prefer_const_declarations, avoid_print
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:vehicle_tracking_mobile_app/location/location.dart';
+import 'package:permission_handler/permission_handler.dart'
+    as permission_handler;
+import 'package:location/location.dart' as loc;
+
+class StartTracking extends StatefulWidget {
+  const StartTracking({Key? key}) : super(key: key);
+
+  @override
+  State<StartTracking> createState() => _StartTrackingState();
+}
+
+class _StartTrackingState extends State<StartTracking> {
+  String serverIP = '';
+  String serverPort = '';
+
+  loc.LocationData? currentLocation;
+  loc.Location location = loc.Location();
+  Socket? socket;
+
+  @override
+  void initState() {
+    super.initState();
+    requestLocationPermission();
+  }
+
+  Future<void> requestLocationPermission() async {
+    final permission_handler.PermissionStatus status =
+        await permission_handler.Permission.location.request();
+    if (status != permission_handler.PermissionStatus.granted) {
+      print("Location Permission is not Granted!, Turn on Your Location");
+    } else {
+      location.onLocationChanged.listen((loc.LocationData? locationData) {
+        setState(() {
+          currentLocation = locationData;
+          sendDataToServer();
+        });
+      });
+    }
+  }
+
+  Future<void> sendDataToServer() async {
+    if (serverIP.isEmpty || serverPort.isEmpty) {
+      print('Please enter server IP and Port');
+      return;
+    }
+
+    try {
+      if (socket == null || socket?.write == null) {
+        // Establish a new TCP connection
+        socket = await Socket.connect(serverIP, int.parse(serverPort));
+      }
+
+      final imei = '356331110282877';
+      final dateTime = DateTime.now().toString();
+      final latitude = currentLocation?.latitude ?? 44.82398238;
+      final longitude = currentLocation?.longitude ?? 23.472798279;
+      final speed = currentLocation?.speed ?? 12.0;
+      final heading = currentLocation?.heading ?? 34.48983;
+      final altitude = currentLocation?.altitude ?? 33.3939274927;
+
+      final dataString =
+          '$imei,$dateTime,$latitude,$longitude,$speed,$heading,$altitude';
+
+      print('Sending data to server: $serverIP:$serverPort, Data: $dataString');
+
+      // Send data to the server
+      socket?.write(dataString);
+
+      // Optionally, wait for a response from the server
+      final response = await socket
+          ?.transform(utf8.decoder as StreamTransformer<Uint8List, dynamic>)
+          .join();
+      print('Server response: $response');
+    } catch (e) {
+      print('Error connecting to the server: $e');
+      socket?.close();
+      socket = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    socket?.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 27, 187, 1),
+        leading: null,
+        actions: [
+          Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext dialogContext) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Server Setting'),
+                            contentPadding: const EdgeInsets.all(16.0),
+                            content: SingleChildScrollView(
+                              child: ListBody(
+                                children: <Widget>[
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        child: TextFormField(
+                                          onChanged: (value) {
+                                            setState(() {
+                                              serverIP = value;
+                                            });
+                                          },
+                                          decoration: const InputDecoration(
+                                            labelText: 'Server IP',
+                                            labelStyle: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 27, 187, 1),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 16),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        child: TextFormField(
+                                          onChanged: (value) {
+                                            setState(() {
+                                              serverPort = value;
+                                            });
+                                          },
+                                          decoration: const InputDecoration(
+                                            labelText: 'Server Port',
+                                            labelStyle: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 27, 187, 1),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      // Perform any validation or saving logic for server IP and Port
+                                      Navigator.of(dialogContext).pop();
+                                    },
+                                    child: const Text('Submit'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+                icon: const Icon(Icons.settings),
+                iconSize: 35,
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: SizedBox(
+                width: 250,
+                child: Image.asset('assets/images/Car.png'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CarLocation(
+                        serverIP: '',
+                        serverPort: '',
+                      ),
+                    ),
+                  );
+                },
+                child: const CircleAvatar(
+                  radius: 60.0,
+                  backgroundColor: Color.fromARGB(255, 27, 187, 1),
+                  child: Text(
+                    "\t\t\tStart \nTracking",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
